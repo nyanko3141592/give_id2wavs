3b6dd26e065c252c89127ea8f5d8bec9a2f1dc73
nya3_neko2 <takahashinaoki521@gmail.com>
2022-07-01 13:14:53 +0900

    音声生成部分を修正

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+import string
 import env
 import glob
 import os
+import json
+from typing import List, Dict
 
 import utils.check_sentence
 from utils.check_files import check_lacked_files
 from utils.voice2text import v2t
+
+wav_sentence_json: string = "wav_sentence.json"
 
 # get files in input dir
 wav_files = glob.glob(os.path.join(env.master_dir, env.input_dir_name, '*.wav'))
@@ -12,9 +17,24 @@ print(f"wav_files: {len(wav_files)}")
 # text file 作成
 text_file = open(os.path.join(env.master_dir, env.rename_failed_file), 'w')
 
+# wav to speech recognition text
+wav_sentence_dict: Dict[str, str] = {}
+if os.path.exists(os.path.join(env.master_dir, wav_sentence_json)):
+    with open(os.path.join(env.master_dir, wav_sentence_json), 'r') as f:
+        wav_sentence_dict = json.load(f)
+l: List[str] =  sorted(list(set(wav_files) -  set(wav_sentence_dict.keys())))
+for wav_file in l:
+    wav_sentence_dict[wav_file] = v2t(wav_file)
+    print(f"{wav_file}: {wav_sentence_dict[wav_file]}")
+    # save dict as json
+    with open(os.path.join(env.master_dir, wav_sentence_json), 'w') as f:
+        json.dump(wav_sentence_dict, f, ensure_ascii=False, indent=4)
+
+
 # 音声ファイルのrename
-for wav_file in wav_files:
-    voice_text = v2t(wav_file)
+l = sorted(wav_sentence_dict.keys())
+for wav_file in l:
+    voice_text = wav_sentence_dict[wav_file]
     id, text, index, ratio = utils.check_sentence.search_id(voice_text)
     print(f"id: {id}, voice: {voice_text}\n{' ' * (len(id) + 4)}current: {text}")
     # 閾値を超えた一致率のファイルをrename
